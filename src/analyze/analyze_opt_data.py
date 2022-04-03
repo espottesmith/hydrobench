644694074559c6b0f71bf606fd5d0c76c7bc9535
Alex Epstein <aepstein@lbl.gov>
2022-04-03 10:11:18 -0700

    colorful heatmaps

--- src/analyze/analyze_opt_data.py
+++ src/analyze/analyze_opt_data.py
@@ -11,13 +11,17 @@ from pandas import DataFrame as df
 from pymongo import MongoClient
 
 from fireworks.core.launchpad import LaunchPad
-from pymatgen.analysis.molecule_matcher import MoleculeMatcher, \
-    KabschMatcher, BruteForceOrderMatcher
+from pymatgen.analysis.molecule_matcher import (
+    MoleculeMatcher,
+    KabschMatcher,
+    BruteForceOrderMatcher,
+)
 from pymatgen.core.structure import Molecule
-from pymatgen.core.units import Ha_to_eV
+
+# from pymatgen.core.units import Ha_to_eV
 
 pio.renderers.default = "browser"
-Ha_to_eV = 627.50961
+Ha_to_eV = 27.211
 kcal_to_Ha = 1 / Ha_to_eV
 data_dir = "../../../20220301_opt_benchmark"
 
@@ -44,12 +48,10 @@ def plot_heatmap(data, title="", zmin=None, zmax=None):
     )
 
     fig.update_yaxes(
-        autorange="reversed", visible=True, showgrid=False,
-        nticks=len(dataframe.index)
+        autorange="reversed", visible=True, showgrid=False, nticks=len(dataframe.index)
     )
     fig.update_xaxes(side="top", visible=True, showgrid=False)
-    fig.update_layout(width=800, height=800, plot_bgcolor="#808080",
-                      title=title)
+    fig.update_layout(width=800, height=800, plot_bgcolor="#808080", title=title)
     # fig.write_image("mae_heatmap_kcal.pdf")
     fig.show()
 
@@ -92,6 +94,9 @@ def get_elec_mae(reference, target):
 
             mae[moltype + "_fwd"][method] = abs(fwd_e - ref_fwd_e) * Ha_to_eV
             mae[moltype + "_rev"][method] = abs(rev_e - ref_rev_e) * Ha_to_eV
+            #
+            # mae[moltype + "_fwd"][method] = abs(fwd_e - ref_fwd_e)
+            # mae[moltype + "_rev"][method] = abs(rev_e - ref_rev_e)
 
             target_barriers[moltype + "_fwd"][method] = abs(fwd_e) * Ha_to_eV
             target_barriers[moltype + "_rev"][method] = abs(rev_e) * Ha_to_eV
@@ -106,7 +111,7 @@ def get_elec_mae(reference, target):
 
         avg_mae = np.sum(all_mae) / len(all_mae)
         mae["average"][method] = avg_mae
-    return (mae)
+    return mae
 
 
 def get_elec_mse(reference, target):
@@ -161,7 +166,7 @@ def get_elec_mse(reference, target):
 
         avg_mae = np.sum(all_mae) / len(all_mae)
         mae["average"][method] = avg_mae
-    return (mae)
+    return mae
 
 
 def get_cost(data):
@@ -226,17 +231,17 @@ def get_thermochemistry_maes(reference, target):
         ref_ts_e = reference[moltype]["ts"]["total_corrected"]
         ref_ts_s = reference[moltype]["ts"]["entropy"] / 1000 * kcal_to_Ha
         ref_ts_h = reference[moltype]["ts"]["enthalpy"] * kcal_to_Ha
-        ref_ts_g = ref_ts_h + ref_ts_e - ref_ts_s * temp
+        ref_ts_g = ref_ts_e + ref_ts_h  - ref_ts_s * temp
 
         ref_rct_e = reference[moltype]["rct"]["total_corrected"]
         ref_rct_s = reference[moltype]["rct"]["entropy"] / 1000 * kcal_to_Ha
         ref_rct_h = reference[moltype]["rct"]["enthalpy"] * kcal_to_Ha
-        ref_rct_g = ref_rct_e - ref_rct_s * temp + ref_rct_h
+        ref_rct_g = ref_rct_e + ref_rct_h - ref_rct_s * temp
 
         ref_pro_e = reference[moltype]["pro"]["total_corrected"]
         ref_pro_s = reference[moltype]["pro"]["entropy"] / 1000 * kcal_to_Ha
         ref_pro_h = reference[moltype]["pro"]["enthalpy"] * kcal_to_Ha
-        ref_pro_g = ref_pro_e - ref_pro_s * temp + ref_pro_h
+        ref_pro_g = ref_pro_e + ref_pro_h - ref_pro_s * temp
 
         ref_fwd_g = ref_ts_g - ref_rct_g
         ref_fwd_s = ref_ts_s - ref_rct_s
@@ -270,19 +275,13 @@ def get_thermochemistry_maes(reference, target):
             rev_h = ts_h - pro_h
             rev_s = ts_s - pro_s
 
-            g_mae[moltype + "_fwd"][method] = abs(
-                ref_fwd_g - fwd_g) * Ha_to_eV
-            s_mae[moltype + "_fwd"][method] = abs(
-                ref_fwd_s - fwd_s) * Ha_to_eV
-            h_mae[moltype + "_fwd"][method] = abs(
-                ref_fwd_h - fwd_h) * Ha_to_eV
+            g_mae[moltype + "_fwd"][method] = abs(ref_fwd_g - fwd_g) * Ha_to_eV
+            s_mae[moltype + "_fwd"][method] = abs(ref_fwd_s - fwd_s) * Ha_to_eV
+            h_mae[moltype + "_fwd"][method] = abs(ref_fwd_h - fwd_h) * Ha_to_eV
 
-            g_mae[moltype + "_rev"][method] = abs(
-                ref_rev_g - rev_g) * Ha_to_eV
-            s_mae[moltype + "_rev"][method] = abs(
-                ref_rev_s - rev_s) * Ha_to_eV
-            h_mae[moltype + "_rev"][method] = abs(
-                ref_rev_h - rev_h) * Ha_to_eV
+            g_mae[moltype + "_rev"][method] = abs(ref_rev_g - rev_g) * Ha_to_eV
+            s_mae[moltype + "_rev"][method] = abs(ref_rev_s - rev_s) * Ha_to_eV
+            h_mae[moltype + "_rev"][method] = abs(ref_rev_h - rev_h) * Ha_to_eV
 
     g_mae["average"] = {}
     h_mae["average"] = {}
@@ -376,19 +375,13 @@ def get_thermochemistry_rmaes(reference, target):
             rev_h = ts_h - pro_h
             rev_s = ts_s - pro_s
 
-            g_mae[moltype + "_fwd"][method] = abs(
-                ref_fwd_g - fwd_g) / ref_fwd_g
-            s_mae[moltype + "_fwd"][method] = abs(
-                ref_fwd_s - fwd_s) / ref_fwd_s
-            h_mae[moltype + "_fwd"][method] = abs(
-                ref_fwd_h - fwd_h) / ref_fwd_h
+            g_mae[moltype + "_fwd"][method] = abs(ref_fwd_g - fwd_g) / ref_fwd_g
+            s_mae[moltype + "_fwd"][method] = abs(ref_fwd_s - fwd_s) / ref_fwd_s
+            h_mae[moltype + "_fwd"][method] = abs(ref_fwd_h - fwd_h) / ref_fwd_h
 
-            g_mae[moltype + "_rev"][method] = abs(
-                ref_rev_g - rev_g) / ref_rev_g
-            s_mae[moltype + "_rev"][method] = abs(
-                ref_rev_s - rev_s) / ref_rev_s
-            h_mae[moltype + "_rev"][method] = abs(
-                ref_rev_h - rev_h) / ref_rev_h
+            g_mae[moltype + "_rev"][method] = abs(ref_rev_g - rev_g) / ref_rev_g
+            s_mae[moltype + "_rev"][method] = abs(ref_rev_s - rev_s) / ref_rev_s
+            h_mae[moltype + "_rev"][method] = abs(ref_rev_h - rev_h) / ref_rev_h
 
     g_mae["average"] = {}
     for method in g_mae["carbonate_fwd"].keys():
@@ -400,7 +393,7 @@ def get_thermochemistry_rmaes(reference, target):
 
         avg_mae = np.sum(all_mae) / len(all_mae)
         g_mae["average"][method] = avg_mae
-    return (g_mae)
+    return g_mae
 
 
 def get_thermochemistry_mses(reference, target):
@@ -472,19 +465,13 @@ def get_thermochemistry_mses(reference, target):
             rev_h = ts_h - pro_h
             rev_s = ts_s - pro_s
 
-            g_mae[moltype + "_fwd"][method] = (
-                                                      ref_fwd_g - fwd_g) * Ha_to_eV
-            s_mae[moltype + "_fwd"][method] = (
-                                                      ref_fwd_s - fwd_s) * Ha_to_eV
-            h_mae[moltype + "_fwd"][method] = (
-                                                      ref_fwd_h - fwd_h) * Ha_to_eV
+            g_mae[moltype + "_fwd"][method] = (ref_fwd_g - fwd_g) * Ha_to_eV
+            s_mae[moltype + "_fwd"][method] = (ref_fwd_s - fwd_s) * Ha_to_eV
+            h_mae[moltype + "_fwd"][method] = (ref_fwd_h - fwd_h) * Ha_to_eV
 
-            g_mae[moltype + "_rev"][method] = (
-                                                      ref_rev_g - rev_g) * Ha_to_eV
-            s_mae[moltype + "_rev"][method] = (
-                                                      ref_rev_s - rev_s) * Ha_to_eV
-            h_mae[moltype + "_rev"][method] = (
-                                                      ref_rev_h - rev_h) * Ha_to_eV
+            g_mae[moltype + "_rev"][method] = (ref_rev_g - rev_g) * Ha_to_eV
+            s_mae[moltype + "_rev"][method] = (ref_rev_s - rev_s) * Ha_to_eV
+            h_mae[moltype + "_rev"][method] = (ref_rev_h - rev_h) * Ha_to_eV
 
     g_mae["average"] = {}
     h_mae["average"] = {}
@@ -525,12 +512,9 @@ def get_rmsds(reference, target):
         promm = KabschMatcher(ref_pro_mol)
         # mm = MoleculeMatcher()
         for method, calc in methods.items():
-            ts_mol = Molecule.from_dict(calc["ts"]["output"][
-                                            "optimized_molecule"])
-            rct_mol = Molecule.from_dict(calc["rct"]["output"][
-                                             "optimized_molecule"])
-            pro_mol = Molecule.from_dict(calc["pro"]["output"][
-                                             "optimized_molecule"])
+            ts_mol = Molecule.from_dict(calc["ts"]["output"]["optimized_molecule"])
+            rct_mol = Molecule.from_dict(calc["rct"]["output"]["optimized_molecule"])
+            pro_mol = Molecule.from_dict(calc["pro"]["output"]["optimized_molecule"])
 
             # ts_rmsd = mm.get_rmsd(ts_mol, ref_ts_mol)
             # rct_rmsd = mm.get_rmsd(rct_mol, ref_rct_mol)
@@ -560,16 +544,13 @@ def get_svpd_with_sp_corrections(data):
         for method, calc in methods.items():
             if "ts_sp" in calc.keys():
                 ts_e = calc["ts_sp"]["output"]["final_energy"]
-                spcorr_data[moltype][method]["ts"]["output"][
-                    "final_energy"] = ts_e
+                spcorr_data[moltype][method]["ts"]["output"]["final_energy"] = ts_e
                 rct_e = calc["rct_sp"]["output"]["final_energy"]
-                spcorr_data[moltype][method]["rct"]["output"]["final_energy"] = \
-                    rct_e
+                spcorr_data[moltype][method]["rct"]["output"]["final_energy"] = rct_e
                 pro_e = calc["pro_sp"]["output"]["final_energy"]
-                spcorr_data[moltype][method]["pro"]["output"]["final_energy"] = \
-                    pro_e
+                spcorr_data[moltype][method]["pro"]["output"]["final_energy"] = pro_e
             else:
-                print('Missing single point for {},{}'.format(moltype, method))
+                print("Missing single point for {},{}".format(moltype, method))
                 spcorr_data[moltype].pop(method)
 
     return spcorr_data
@@ -584,27 +565,27 @@ with open(os.path.join(data_dir, "opt_data_dump_svpd.json")) as json_file:
     svpd_data = json.load(json_file)
 
 elec_mae = get_elec_mae(reference=mp2_ref, target=tzvpd_data)
-# plot_heatmap(elec_mae)
-print('Elec MAE TZVPD')
-dumpfn(elec_mae["average"], fn='../data/elec_mae_tzvpd_opts.json')
-g_mae, h_mae, s_mae = get_thermochemistry_maes(reference=mp2_ref,
-                                               target=tzvpd_data)
+plot_heatmap(elec_mae, title='E_MAE_TZVPPD')
+print("Elec MAE TZVPD")
+dumpfn(elec_mae["average"], fn="../data/elec_mae_tzvpd_opts.json")
+g_mae, h_mae, s_mae = get_thermochemistry_maes(reference=mp2_ref, target=tzvpd_data)
 # print('G, H, S MAE TZVPD')
-dumpfn(g_mae["average"], fn='../data/g_mae_tzvpd_opts.json')
-dumpfn(h_mae["average"], fn='../data/h_mae_tzvpd_opts.json')
-dumpfn(s_mae["average"], fn='../data/s_mae_tzvpd_opts.json')
+dumpfn(g_mae["average"], fn="../data/g_mae_tzvpd_opts.json")
+dumpfn(h_mae["average"], fn="../data/h_mae_tzvpd_opts.json")
+dumpfn(s_mae["average"], fn="../data/s_mae_tzvpd_opts.json")
 
-# plot_heatmap(g_mae, title='G_MAE_TZVPPD')
+plot_heatmap(g_mae, title='G_MAE_TZVPPD')
 
 g_rmae = get_thermochemistry_rmaes(reference=mp2_ref, target=tzvpd_data)
 # plot_heatmap(g_rmae, title='G_RMAE_TZVPPD')
-print('G, H, S MAE TZVPD')
-print(g_rmae["average"])
 
-g_mae_svpd = get_thermochemistry_maes(reference=mp2_ref, target=svpd_data)
+g_mae_svpd, h_mae_svpd, s_mae_svpd = get_thermochemistry_maes(
+    reference=mp2_ref, target=svpd_data
+)
 # plot_heatmap(g_mae_svpd, title='G_MAE_SVPD')
-print('G, H, S MAE SVPD')
-print(g_rmae["average"])
+dumpfn(g_mae_svpd["average"], fn="../data/g_mae_svpd_opts.json")
+dumpfn(h_mae_svpd["average"], fn="../data/h_mae_svpd_opts.json")
+dumpfn(s_mae_svpd["average"], fn="../data/s_mae_svpd_opts.json")
 # avg_g_mae_svpd
 
 g_rmae_svpd = get_thermochemistry_rmaes(reference=mp2_ref, target=svpd_data)
@@ -623,19 +604,18 @@ elec_mse = get_elec_mse(reference=mp2_ref, target=spcorr_svpd)
 
 
 g_mae_corrsvpd, h_mae_corrsvpd, s_mae_corrsvpd = get_thermochemistry_maes(
-    reference=mp2_ref, target=spcorr_svpd)
-# plot_heatmap(g_mae_corrsvpd, title='G_MAE_corrrSVPD')
-# plot_heatmap(h_mae_corrsvpd, title='H_MAE_corrrSVPD')
-# plot_heatmap(s_mae_corrsvpd, title='S_MAE_corrrSVPD')
+    reference=mp2_ref, target=spcorr_svpd
+)
+dumpfn(g_mae_corrsvpd["average"], fn="../data/g_mae_corrsvpd_opts.json")
+dumpfn(h_mae_corrsvpd["average"], fn="../data/h_mae_corrsvpd_opts.json")
+dumpfn(s_mae_corrsvpd["average"], fn="../data/s_mae_corrsvpd_opts.json")
+
 #
 g_mse_corrsvpd, h_mse_corrsvpd, s_mse_corrsvpd = get_thermochemistry_mses(
-    reference=mp2_ref, target=spcorr_svpd)
-# plot_heatmap(g_mse_corrsvpd, title='G_MSE_corrrSVPD')
-# plot_heatmap(h_mse_corrsvpd, title='H_MSE_corrrSVPD')
-# plot_heatmap(s_mse_corrsvpd, title='S_MSE_corrrSVPD')
+    reference=mp2_ref, target=spcorr_svpd
+)
 
-g_rmae_corrsvpd = get_thermochemistry_rmaes(reference=mp2_ref,
-                                            target=spcorr_svpd)
+g_rmae_corrsvpd = get_thermochemistry_rmaes(reference=mp2_ref, target=spcorr_svpd)
 # plot_heatmap(g_rmae_corrsvpd, title='G_RMAE_corrSVPD')
 
 exit()
